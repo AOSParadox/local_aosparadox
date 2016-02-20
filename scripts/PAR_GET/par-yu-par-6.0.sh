f7cc833dc969ba91316af50bc7687723b727590a
Louis Popi <theh2o64@gmail.com>
2016-02-20 21:53:00 +0100

    scripts: More adjustments

--- scripts/PAR_GET/par-yu-par-6.0.sh
+++ scripts/PAR_GET/par-yu-par-6.0.sh
@@ -10,14 +10,14 @@ function extract() {
         fi
 	cd /home/louis/WORK/YU_CAF/
 	cd $FILE
-	echo >> /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
-	echo "cd /home/louis/WORK/AOSPARADOX_FALCON/ && cd $FILE # Go to PATH" >> /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
+	echo >> /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
+	echo "cd /home/louis/WORK/YU_CAF/ && cd $FILE # Go to PATH" >> /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
 	# Uncomment for TAG Merging
-	git log -n 50 --pretty="git cherry-pick -s %H # %s - %ae" >> /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO
-	tac /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO >> /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO_NEW
-	cat /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO_NEW >> /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
-	rm /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO_NEW
-	echo "git push aosparadox yu-par-6.0 -f" >> /home/louis/WORK/AOSPARADOX_FALCON/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
+	git log -n 50 --pretty="git cherry-pick -s %H # %s - %ae" >> /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO
+	tac /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO >> /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO_NEW
+	cat /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO_NEW >> /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
+	rm /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0_REPO_NEW
+	echo "git push aosparadox yu-par-6.0 -f" >> /home/louis/WORK/YU_CAF/upgrade/scripts/COMMITS/CHANGELOG-yu-par-6.0
 	cd /home/louis/WORK/YU_CAF/
     done
 }
