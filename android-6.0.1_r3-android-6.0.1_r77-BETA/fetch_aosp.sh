31e4fcfb702deb97a94ca632f86769dd9790e79b
Louis Popi <theh2o64@gmail.com>
2017-02-11 18:41:33 +0100

    android-6.0.1_r77-BETA: Update patches for new BETA tag

--- android-6.0.1_r3-android-6.0.1_r77-BETA/fetch_aosp.sh
+++ android-6.0.1_r3-android-6.0.1_r77-BETA/fetch_aosp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export MY_NICE_FOLDER=$(pwd)
+export PATCHES=$MY_NICE_FOLDER/local_aosparadox/android-6.0.1_r3-android-6.0.1_r77-STABLE/patches
 
 function extract() {
     for FILE in `egrep -v '(^#|^$)' $1`; do
@@ -19,4 +20,4 @@ function extract() {
     done
 }
 
-extract project_list
+extract $PATCHES/../project_list
