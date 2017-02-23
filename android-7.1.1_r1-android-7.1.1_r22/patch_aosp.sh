c20b2d5500e62dd7ecd164234b9867458fb3ba92
Louis Popi <theh2o64@gmail.com>
2017-02-23 10:43:36 +0100

    patches: Add android-7.1.1_r1-android-7.1.1_r22 patches

---
+++ android-7.1.1_r1-android-7.1.1_r22/patch_aosp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export MY_NICE_FOLDER=$(pwd)
+export PATCHES=$MY_NICE_FOLDER/local_aosparadox/android-7.1.1_r1-android-7.1.1_r22/patches
+
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+	cd $MY_NICE_FOLDER
+	cd $FILE
+	git am -3 $PATCHES/$FILE/*
+	cd $MY_NICE_FOLDER
+    done
+}
+
+extract $PATCHES/../project_list
